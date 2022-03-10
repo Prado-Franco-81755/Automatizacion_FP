0f7f268d86fe412e7da3256f771b5fdc23933de7
Prado-Franco-81755 <franpr1144@gmail.com>
2022-03-10 10:55:56 -0300

    commiteado

--- src/main/java/Utils/BaseTest.java
+++ src/main/java/Utils/BaseTest.java
@@ -3,8 +3,8 @@ package Utils;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
-//import org.testng.Reporter;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class BaseTest {
 	public static WebDriver iniciarDriver(String url, String browserName){
@@ -16,12 +16,12 @@ public class BaseTest {
 				driver = new ChromeDriver();
 				break;
 				}
-			//case "FIREFOX": {
-				//System.setProperty("webdriver.gecko.driver", "lugar donde esta");
-				//System.out.println("Abro browser Firefox");
-				//driver = new FirefoxDriver();
-				//break;
-				//}
+			case "FIREFOX": {
+				System.setProperty("webdriver.gecko.driver", "C:/ChromeDrivers/geckodriver.exe");
+				System.out.println("Abro browser Firefox");
+				driver = new FirefoxDriver();
+				break;
+				}
 			default : {
 				System.out.println("No se seleeciono ningun browser correcto, se le asignara Chrome");
 				System.setProperty("webdriver.chrome.driver", "lugar donde esta");
