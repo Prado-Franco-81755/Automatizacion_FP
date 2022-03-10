01737e9bdff99c1b3515fff47d02078202fb67a7
Prado-Franco-81755 <franpr1144@gmail.com>
2022-03-10 11:51:34 -0300

    commiteado

--- src/test/java/ListadoWiki/ListadoWiki.java
+++ src/test/java/ListadoWiki/ListadoWiki.java
@@ -19,12 +19,15 @@ public class ListadoWiki {
 	
 	@Test(dataProvider = "Data Provider de Wiki", description = "Validar que las busquedas en Wikipedia funcionan")
 	public void ValidarBusquedaWikipedia(String searchText) throws Exception{
-		//La verdad profe, no se para que puse esto, pero siento que deberia agregar algo abajo
-		//aunque no haga nada
+
 		System.setProperty("webdriver.chrome.driver", "C:/ChromeDrivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://wikipedia.org");
 		WebElement searchInput = driver.findElement(By.id("searchInput"));		
 		Assert.assertTrue(searchInput.isDisplayed(), "El elemento esta visible");
+		searchInput.sendKeys(searchText);
+		searchInput.submit();
+		Thread.sleep(1500);
+		driver.close();
 	}
 }
